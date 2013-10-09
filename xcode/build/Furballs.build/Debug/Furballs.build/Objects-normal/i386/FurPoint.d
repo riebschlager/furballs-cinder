cb0f42002b2b02e80e6a6a4b2b34c9fe22f0a3ce
Chris Riebschlager <riebschlager@gmail.com>
2013-10-09 08:28:05 -0500

    Delete build folder.

--- xcode/build/Furballs.build/Debug/Furballs.build/Objects-normal/i386/FurPoint.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/criebsch/Documents/Cinder/furballs-cinder/src/FurPoint.cpp \
-  /Users/criebsch/Documents/Cinder/furballs-cinder/src/FurPoint.h \
-  ../../../../Documents/Frameworks/cinder_0.8.5_mac/include/cinder/Perlin.h \
-  ../../../../Documents/Frameworks/cinder_0.8.5_mac/include/cinder/Rand.h
--- xcode/build/Furballs.build/Debug/Furballs.build/Objects-normal/i386/Point.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/criebsch/Desktop/Furballs/src/Point.cpp \
-  /Users/criebsch/Desktop/Furballs/src/Point.h
